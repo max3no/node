2a47ed159e84c15ed72d90c39d67a808b56d8720
ATMECS\Vaibhav.Singh <vaibhav.singh@atmecs.com>
2018-07-04 16:19:26 +0530

    Express mongoose

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//connect mongo
+mongoose.connect("mongodb://vai:reddevil333@ds125831.mlab.com:25831/empl")
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Connection to database failed "+e);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false} ));
+
+//cors filter
+app.use((req, res, next) => {
+
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+module.exports = app;
